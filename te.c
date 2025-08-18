368d20719391c29d40b9999de14e5ec5538a09de
Philipp Tanlak <philipp.tanlak@gmail.com>
2025-08-18 22:05:50 +0200

    3

--- te.c
+++ te.c
@@ -44,10 +44,14 @@ void debug(const char *fmt, ...)
 		      (N), (unsigned long long)_elapsed_ns, (unsigned long long)(_elapsed_ns / (N)));                  \
 	} while (0)
 
-struct arena {
-	char  *buf;
-	size_t size;
-	size_t used;
+enum editor_mode {
+	EDITOR_MODE_NORMAL,
+	EDITOR_MODE_INSERT,
+};
+
+struct editor_state {
+	int		 cursor_pos;
+	enum editor_mode mode;
 };
 
 #define TERMINAL_MODE_ALTERNATE "\e[?1049h", 8
@@ -55,6 +59,8 @@ struct arena {
 #define TERMINAL_CURSOR_HIDE	"\e[?25l", 6
 #define TERMINAL_CURSOR_SHOW	"\e[?25h", 6
 #define TERMINAL_CURSOR_RESET	"\e[1;1H", 6
+#define TERMINAL_CURSOR_BLOCK	"\e[2 q", 5
+#define TERMINAL_CURSOR_BAR	"\e[6 q", 5
 #define TERMINAL_FRAME_START	"\e[?25l\e[1;1H", 12
 #define TERMINAL_FRAME_END	"\e[1;1H\e[?25h", 12
 
@@ -128,6 +134,17 @@ int terminal_get_window_size(struct terminal_config *term, int *rows, int *cols)
 	return 0;
 }
 
+int terminal_place_cursor(struct terminal_config *term, int row, int col)
+{
+
+	char buf[32] = {};
+	int  buf_len = snprintf(buf, sizeof(buf), "\e[%d;%dH", row + 1, col + 1);
+	if (write(term->fd, buf, buf_len) == -1) {
+		return -1;
+	}
+	return 0;
+}
+
 struct bounds {
 	int row;
 	int col;
@@ -193,6 +210,7 @@ struct file_buffer {
 
 int  file_buffer_init_from_file(struct file_buffer *file, char *pathname);
 void file_buffer_get_str(struct file_buffer *file, char **str, int *str_len);
+void file_buffer_render_to_context(struct file_buffer *file, struct render_context *ctx, struct bounds *bounds);
 void file_buffer_cleanup(struct file_buffer *file);
 
 int file_buffer_init_from_file(struct file_buffer *file, char *pathname)
@@ -282,7 +300,8 @@ void file_buffer_render_to_context(struct file_buffer *file, struct render_conte
 				if (c == '\n') {
 					found_newline = true;
 					str_ofs++; // consume the newline
-					// rest of row in bounds will be whatever was already in screen_buffer
+						   // rest of row in bounds will be whatever was already in
+						   // screen_buffer
 				}
 				else {
 					int screen_idx		       = screen_row * ctx->cols + screen_col;
@@ -291,6 +310,30 @@ void file_buffer_render_to_context(struct file_buffer *file, struct render_conte
 				}
 			}
 			// if found_newline is true, we leave whatever was already in screen_buffer
+		}
+	}
+
+	free(str);
+}
+
+void file_buffer_get_cursor_pos(struct file_buffer *file, int pos, int *row, int *col)
+{
+	char *str     = (char *)rope_create_cstr(file->rope);
+	int   str_len = rope_char_count(file->rope);
+
+	*row = 0;
+	*col = 0;
+
+	if (pos > str_len) {
+		free(str);
+		return;
+	}
+
+	for (int i = 0; i < pos; i++) {
+		(*col)++;
+		if (str[i] == '\n') {
+			(*row)++;
+			*col = 0;
 		}
 	}
 
@@ -373,27 +416,12 @@ void status_line_render_to_context(struct status_line *sl, struct render_context
 	}
 }
 
-struct editor_state {
-	int cursor_row;
-	int cursor_col;
-};
-
-int editor_state_place_cursor(struct editor_state *state, int fd)
-{
-	char buf[32] = {};
-	int  buf_len = snprintf(buf, sizeof(buf), "\e[%d;%dH", state->cursor_row + 1, state->cursor_col + 1);
-	if (write(fd, buf, buf_len) == -1) {
-		return -1;
-	}
-	return 0;
-}
-
 int main(void)
 {
 	struct terminal_config term	    = {};
 	struct render_context  render_ctx   = {};
 	struct file_buffer     file_buffer  = {};
-	struct editor_state    editor_state = {};
+	struct editor_state    editor_state = {.mode = EDITOR_MODE_NORMAL};
 
 	if (terminal_init(&term) == -1) {
 		err(EXIT_FAILURE, "terminal init");
@@ -410,15 +438,11 @@ int main(void)
 		err(EXIT_FAILURE, "file buffer init");
 	}
 
-	render_context_clear(&render_ctx);
-
 	struct bounds file_buffer_bounds;
 	file_buffer_bounds.col	  = 0;
 	file_buffer_bounds.row	  = 0;
 	file_buffer_bounds.width  = term.window_cols;
 	file_buffer_bounds.height = term.window_rows - 2;
-
-	file_buffer_render_to_context(&file_buffer, &render_ctx, &file_buffer_bounds);
 
 	struct bounds status_line_bounds;
 	status_line_bounds.col	  = 0;
@@ -434,37 +458,63 @@ int main(void)
 	status_line.cursor_row = 10;
 	status_line.cursor_col = 50;
 
-	status_line_render_to_context(&status_line, &render_ctx, &status_line_bounds);
-	render_context_render(&render_ctx, term.fd);
+	int row, col;
 
 	for (;;) {
+		file_buffer_get_cursor_pos(&file_buffer, editor_state.cursor_pos, &row, &col);
+
+		render_context_clear(&render_ctx);
+		file_buffer_render_to_context(&file_buffer, &render_ctx, &file_buffer_bounds);
+		status_line_render_to_context(&status_line, &render_ctx, &status_line_bounds);
+		render_context_render(&render_ctx, term.fd);
+
+		terminal_place_cursor(&term, row, col);
+
 		char c;
 		if (read(term.fd, &c, 1) == -1) {
 			err(EXIT_FAILURE, "read input");
 		}
 
-		bool should_exit = false;
+		debug("keypress: %d\n", c);
 
-		switch (c) {
-		case 'h':
-			editor_state.cursor_col = MAX(0, editor_state.cursor_col - 1);
-			editor_state_place_cursor(&editor_state, term.fd);
-			break;
-		case 'j':
-			editor_state.cursor_row = MIN(file_buffer_bounds.height - 1, editor_state.cursor_row + 1);
-			editor_state_place_cursor(&editor_state, term.fd);
-			break;
-		case 'k':
-			editor_state.cursor_row = MAX(0, editor_state.cursor_row - 1);
-			editor_state_place_cursor(&editor_state, term.fd);
-			break;
-		case 'l':
-			editor_state.cursor_col = MIN(file_buffer_bounds.width - 1, editor_state.cursor_col + 1);
-			editor_state_place_cursor(&editor_state, term.fd);
-			break;
-		case 'q':
-			should_exit = true;
-			break;
+		bool should_exit = false;
+		int  char_count	 = rope_char_count(file_buffer.rope);
+		char str[2]	 = {c, 0};
+
+		if (editor_state.mode == EDITOR_MODE_NORMAL) {
+			switch (c) {
+			case 'h':
+				editor_state.cursor_pos = MAX(0, editor_state.cursor_pos - 1);
+				break;
+			case 'j':
+				break;
+			case 'k':
+				break;
+			case 'l':
+				editor_state.cursor_pos = MIN(char_count, editor_state.cursor_pos + 1);
+				break;
+			case 'i':
+				editor_state.mode = EDITOR_MODE_INSERT;
+				write(term.fd, TERMINAL_CURSOR_BAR);
+				break;
+			case 'q':
+				should_exit = true;
+				break;
+			}
+		}
+		else {
+			switch (c) {
+			case '\e':
+				editor_state.mode = EDITOR_MODE_NORMAL;
+				write(term.fd, TERMINAL_CURSOR_BLOCK);
+				break;
+			case 127:
+				rope_del(file_buffer.rope, --editor_state.cursor_pos, 1);
+				break;
+			default:
+				rope_insert(file_buffer.rope, editor_state.cursor_pos++, (uint8_t *)str);
+				break;
+			}
 		}
 
 		if (should_exit) {
